48ff043de0f2dc4e9a4ec4f8bc9c3af7441d3b08
COMAXE <95748314+Galaxy-moi@users.noreply.github.com>
2022-01-11 11:02:20 +0100

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,38 @@
+//let x = prompt("Quel est le nombre d'euro a convertir ?");
+//let somme_dollars = x * 1.13
+//if (somme_dollars > 15) {
+//   alert("on prend");
+//} else {
+//    alert("on laisse c'est trop chère");
+//}
+
+
+//let xx=prompt("que vaut xx ?");
+//let y=prompt("que vaut y ?");
+//let z=prompt("que vaut z?");
+//alert(x^2 + (3 * y) * z);
+
+
+//for (var iter = 0; iter < 1000; iter = iter+10) {
+//   alert('Tu as genere  ' + iter + '  de gemmes');
+//   }
+
+//let i = 0;
+//while (i<10){
+//   alert("hello " + i);
+//   i = i + 1
+//}
+
+//function carre(i){
+//   return(i*i);
+
+//    }
+
+//let x = prompt("Que veux-tu calculer ?");
+//let somme = x ^ 2 - (3 * x) + 1
+//alert(somme);
+function f(x){
+    return(x**2 - (3*x)+ 1)
+
+}
+document.write(f(e));
